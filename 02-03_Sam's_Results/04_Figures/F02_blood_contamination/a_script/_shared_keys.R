6d4ea844d6f7956e970457dbfa4e903304bba40f
Dustyn-T-Lewis <dtlts14@gmail.com>
2026-05-15 12:50:28 -0400

    fix(F02): extend normalize_sample_id to handle PPS samples

--- 02-03_Sam's_Results/04_Figures/F02_blood_contamination/a_script/_shared_keys.R
+++ 02-03_Sam's_Results/04_Figures/F02_blood_contamination/a_script/_shared_keys.R
@@ -3,7 +3,9 @@
 # CR6_T1. Always canonicalize to the short form before joining.
 
 normalize_sample_id <- function(x) {
-  sub("^CR0*([0-9]+)_T([12])$", "CR\\1_T\\2", x)
+  x <- sub("^CR0*([0-9]+)_T([12])$", "CR\\1_T\\2", x)
+  x <- sub("^PPS0*([0-9]+)$", "PPS\\1", x)
+  x
 }
 
 # Quick self-check (only runs if sourced as main).
@@ -12,7 +14,10 @@ if (sys.nframe() == 0) {
     normalize_sample_id("CR006_T1") == "CR6_T1",
     normalize_sample_id("CR6_T1")   == "CR6_T1",
     normalize_sample_id("CR10_T2")  == "CR10_T2",
-    normalize_sample_id("CR017_T2") == "CR17_T2"
+    normalize_sample_id("CR017_T2") == "CR17_T2",
+    normalize_sample_id("PPS02")    == "PPS2",
+    normalize_sample_id("PPS2")     == "PPS2",
+    normalize_sample_id("PPS44")    == "PPS44"
   )
   message("_shared_keys.R: self-check passed")
 }
